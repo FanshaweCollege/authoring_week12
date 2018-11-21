811fd1d10c57b075117169f770d9e18f00c18c36
annapancioli <annapancioli@gmail.com>
2018-11-21 10:01:33 -0500

    edited all files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(()=>{
+
+    const vm = new VTTCue({
+        el: '#app',
+
+        data:{
+            mainmessage: "welcome to my video app!"
+        },
+
+        created: function(){
+
+        },
+
+        methods:{
+
+        }
+
+    });
+
+})();
